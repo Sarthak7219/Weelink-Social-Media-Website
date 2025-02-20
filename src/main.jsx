c585011502f251abaa5636127d6752d82bfa3db8
Sarthak <sarthakrangari788@gmail.com>
2025-02-20 17:52:45 +0530

    path resolved

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./static/css/socialv.css?v=4.0.0";
 import "./static/css/messages.css";
 import "./static/vendor/@fortawesome/fontawesome-free/css/all.min.css";
 import "./static/vendor/remixicon/fonts/remixicon.css";
-import "./static/vendor/vanillajs-datepicker/dist/css/datepicker.min.css";
+import "./static/vendor/vanillajs-datepicker/datepicker.min.css";
 import "./static/vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
